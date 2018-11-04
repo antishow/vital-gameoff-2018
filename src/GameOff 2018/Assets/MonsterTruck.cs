fd72023875be2ca21149ca5e380fe1131cdcdad0
Matthew Chase <matt@vtldesign.com>
2018-11-04 15:33:57 -0500

    Add player and a too-basic follow script

--- src/GameOff 2018/Assets/MonsterTruck.cs
+++ src/GameOff 2018/Assets/MonsterTruck.cs
@@ -1,16 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MonsterTruck : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private GameObject player;
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player");
+        navMeshAgent = (NavMeshAgent)GetComponent("NavMeshAgent");
+    }
+
+    // Use this for initialization
+    void Start () {
+        if (player != null) {
+            InvokeRepeating("TargetPlayerLocation", 0, 2.0f);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void TargetPlayerLocation()
+    {
+        Vector3 target = player.transform.position;
+        navMeshAgent.SetDestination(player.transform.position);
+    }
 }
