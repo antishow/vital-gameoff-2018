45822bbe8701e8cdddf92f287a32307e40f19ae6
Matthew Chase <matt@vtldesign.com>
2018-11-06 20:51:28 -0500

    Starting Collectible code

---
+++ src/GameOff 2018/Assets/Collectible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour {
+    public int PointValue;
+    public GameObject CollectionEffect;
+
+    public delegate void CollectedEvent();
+    public event CollectedEvent OnCollect;
+
+    private void OnTriggerEnter(Collider other){
+        if (other.tag == "Player" && OnCollect != null) {
+            gameObject.SetActive(false);
+            GameObject effect = Instantiate(CollectionEffect, transform.position, Quaternion.identity, transform.parent);
+            OnCollect();
+            Destroy(effect, 3.0f);
+            Destroy(this, 3.1f);
+        }
+    }
+}
---
+++ src/GameOff 2018/Assets/Dot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+    private Collectible collectible;
+
+    private void Awake() {
+        collectible = GetComponent<Collectible>();
+        collectible.OnCollect += OnCollect;
+    }
+
+    void OnCollect() {
+        Debug.Log("MONCH");
+    }
+}
