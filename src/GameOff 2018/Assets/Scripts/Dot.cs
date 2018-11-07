426f2be462763f12cb1af7308e999153bb8e28bf
Matthew Chase <matt@vtldesign.com>
2018-11-07 07:21:17 -0500

    Add some animation to the dot

--- src/GameOff 2018/Assets/Scripts/Dot.cs
+++ src/GameOff 2018/Assets/Scripts/Dot.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 [RequireComponent(typeof(Collectible))]
 public class Dot : MonoBehaviour {
     private Collectible collectible;
+    private Animator animator;
 
     private void Awake() {
         collectible = GetComponent<Collectible>();
         collectible.OnCollect += OnCollect;
+
+        animator = GetComponent<Animator>();
+        float wave = 4.0f;
+        int index = transform.GetSiblingIndex();
+
+        AnimatorStateInfo i = animator.GetCurrentAnimatorStateInfo(0);
+        float clipLength = animator.GetCurrentAnimatorStateInfo(0).length;
+        float offset = Mathf.Repeat(index, wave) / wave;
+
+        animator.Play("Dot", 0, offset);
     }
 
     void OnCollect() {
